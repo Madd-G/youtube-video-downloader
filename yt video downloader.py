0f857a94c2a0cf69b31d18affe7ec83e8e2c6b83
alamsyah <akhmadnuralamsyah@gmail.com>
2020-10-31 11:15:02 +0700

    Add files via upload

--- yt video downloader.py
+++ yt video downloader.py
@@ -1,10 +1,12 @@
+#make sure you have downloaded pytube3 
 from pytube import YouTube
 
-
-yt = YouTube("https://www.youtube.com/watch?v=fi2WkznwWbc")
+# enter the url you want to download
+yt = YouTube("")
 
 print(yt.title)
 
+# to download the highest resolution
 stream = yt.streams.get_highest_resolution().download()
 
 print("Download Done")
